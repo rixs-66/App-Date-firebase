29f7da974ed83a8c3258cb9e4bca6c6ad6d7f3e9
ricalv6 <rica_66@hotmail.es>
2022-03-10 13:36:46 -0600

    App-basica

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import colors from './src/utils/colors';
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import firebase from './src/utils/firebase';
+import 'firebase/auth'
 
 export default function App() {
+
+  const [user, setuser] = useState(undefined);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((response) => {
+      setuser(response);
+    }
+    )
+  }, []);
+  
+
+  if (user === undefined) return null;
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <LinearGradient style={styles.container} colors={[colors.SECONDARY_COLOR_GRADIANT, colors.SECONDARY_COLOR_GRADIANT_DARK]}>
+
+      <SafeAreaView>
+        <StatusBar backgroundColor={colors.SECONDARY_COLOR_GRADIANT} />
+        {user ? <Text style={{color: 'white'}}>Logeado</Text> : <Text style={{color: 'white'}}>no logeado</Text>}
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ src/utils/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCUg460gFgURSoys4GLTpIN1-wZUI8n6G4",
+    authDomain: "date-remenber.firebaseapp.com",
+    projectId: "date-remenber",
+    storageBucket: "date-remenber.appspot.com",
+    messagingSenderId: "421604194521",
+    appId: "1:421604194521:web:e4483b8323c259044ca6c8"
+  };
+
+ export default firebase.initializeApp(firebaseConfig);
